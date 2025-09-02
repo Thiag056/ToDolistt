5fd5bcef38fd6def5e4d1f125358ff18423a4947
Thiag056 <thiagofranciscosantosribeiro@gmail.com>
2025-09-02 16:17:02 -0300

    imports

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,6 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Tarefa {
- private int id;
- private String Titulo;
- private String descricao;
+    private int id;
+    private String Titulo;
+    private String descricao;
+    private boolean completa;
+    private String dataAgora;
 
 }
