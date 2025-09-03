b18f473b9bf8303595e80a16c885388ca62efceb
Thiag056 <thiagofranciscosantosribeiro@gmail.com>
2025-09-03 18:24:02 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Tarefa T = new Tarefa(1, "Estudar Java", "Aprender POO", "03/09/2025");
 
-
+        Tarefaservico serviso = new Tarefaservico();
+        serviso.mostrarTarefa(T);
     }
 }
 
--- src/Tarefaservico.java
+++ src/Tarefaservico.java
@@ -1,5 +1,9 @@
-
 public class Tarefaservico {
-
-
+    public void mostrarTarefa(Tarefa tarefa) {
+        System.out.println("ID: " + tarefa.getId());
+        System.out.println("Título: " + tarefa.getTitulo());
+        System.out.println("Descrição: " + tarefa.getDescricao());
+        System.out.println("Completa: " + tarefa.isCompleta());
+        System.out.println("Data: " + tarefa.getDataAgora());
+    }
 }
