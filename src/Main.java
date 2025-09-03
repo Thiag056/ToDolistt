847e51c8179b702ae034f3cf345a0194a0565be9
Thiag056 <thiagofranciscosantosribeiro@gmail.com>
2025-09-03 20:57:45 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Tarefa T = new Tarefa(1, "Estudar Java", "Aprender POO","03/09/2025",false);
 
-        Tarefaservico serviso = new Tarefaservico();
-        serviso.visualizar(T);
     }
 }
 
--- src/Tarefaservico.java
+++ src/Tarefaservico.java
@@ -1,9 +1,5 @@
+import java.util.Scanner;
+
 public class Tarefaservico {
-    public void visualizar(Tarefa tarefa) {
-        System.out.println("ID: " + tarefa.getId());
-        System.out.println("Título: " + tarefa.getTitulo());
-        System.out.println("Descrição: " + tarefa.getDescricao());
-        System.out.println("Completa: " + tarefa.isCompleta());
-        System.out.println("Data: " + tarefa.getDataAgora());
-    }
+
 }
