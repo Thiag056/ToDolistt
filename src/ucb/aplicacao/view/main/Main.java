a731f6e6b44e771be9f2717c09ab1ccfa8be96af
rodrigo.na <rodrigodiasdonascimento@gmail.com>
2025-09-05 09:45:25 -0300

    divisão de pacotes

--- src/ucb/aplicacao/view/main/Main.java
+++ src/ucb/aplicacao/view/main/Main.java
@@ -1,3 +1,8 @@
+package ucb.aplicacao.view.main;
+
+import ucb.aplicaçao.service.tarefaservice.Tarefaservico;
+import ucb.aplicação.model.tarefas.Tarefa;
+
 public class Main {
     public static void main(String[] args) {
 
