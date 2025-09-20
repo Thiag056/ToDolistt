408c722fe6b2e6afc642b34d17be5f0c083bed90
yanzinzx <yan.magalhaes8586@gmail.com>
2025-09-19 22:58:37 -0300

    criaçaõ switch

--- src/ucb/view/Main.java
+++ src/ucb/view/Main.java
@@ -29,6 +29,48 @@ public class Main {
                 opcao = -1;
             }
 
+            switch (opcao) {
+                case 1:
+                    System.out.print("Título: ");
+                    String titulo = sc.nextLine();
+                    System.out.print("Descrição: ");
+                    String descricao = sc.nextLine();
+                    servico.criar(titulo, descricao);
+                    break;
+                case 2:
+                    servico.listar();
+                    break;
+                case 3:
+                    System.out.print("ID da tarefa para atualizar: ");
+                    int idUpdate = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Novo título: ");
+                    String novoTitulo = sc.nextLine();
+                    System.out.print("Nova descrição: ");
+                    String novaDescricao = sc.nextLine();
+                    servico.atualizar(idUpdate, novoTitulo, novaDescricao);
+                    break;
+                case 4:
+                    System.out.print("ID da tarefa para remover: ");
+                    int idRemove = sc.nextInt();
+                    servico.remover(idRemove);
+                    break;
+                case 5:
+                    System.out.print("ID da tarefa para concluir: ");
+                    int idConcluir = sc.nextInt();
+                    servico.concluir(idConcluir);
+                    break;
+                case 0:
+                    System.out.println(" Saindo...");
+                    break;
+                default:
+                    System.out.println(" Opção inválida! Escolha uma opção de 0 a 5.");
+            }
+        } while (opcao != 0);
+
+        sc.close();
+    }
+}
 
 
 
