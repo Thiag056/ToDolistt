c236d696bd4d94023e6e7365c995af33b510eddb
yanzinzx <yan.magalhaes8586@gmail.com>
2025-09-19 22:57:39 -0300

    crição do menu switch

--- src/ucb/view/Main.java
+++ src/ucb/view/Main.java
@@ -6,7 +6,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tarefaservico servico = new Tarefaservico();
 
-    }
+        int opcao;
+        do {
+            System.out.println("\n=== MENU TO-DO LIST ===");
+            System.out.println("1 - Criar tarefa");
+            System.out.println("2 - Listar tarefas");
+            System.out.println("3 - Atualizar tarefa");
+            System.out.println("4 - Remover tarefa");
+            System.out.println("5 - Concluir tarefa");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha: ");
 
-}
+            try {
+                opcao = sc.nextInt();
+                sc.nextLine();
+            } catch (java.util.InputMismatchException e) {
+                System.out.println(" Entrada inválida. Digite um número.");
+                sc.nextLine();
+                opcao = -1;
+            }
+
+
+
+
